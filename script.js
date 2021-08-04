535017c2f1dc8a5552347f2399897c4cb84ca68a
Bakhtiar Hossain Rana <75600911+bakhtiarhrana@users.noreply.github.com>
2021-08-04 18:31:29 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// Dark mode & light mode functionality
+
+const icon = document.getElementById("icon");
+
+if (localStorage.getItem == null) {
+    localStorage.setItem("theme", "light");
+}
+
+let localData = localStorage.getItem("theme");
+
+// check local storage theme data 
+
+if (localData == "light") {
+    icon.src = "images/moon.png";
+    document.body.classList.remove("dark-theme");
+} else if (localData == "dark") {
+    icon.src = "images/sun.png";
+    document.body.classList.add("dark-theme");
+}
+
+// add or remove theme
+
+icon.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "images/sun.png";
+        localStorage.setItem("theme", "dark");
+    } else {
+        icon.src = "images/moon.png";
+        localStorage.setItem("theme", "light");
+    }
+}
